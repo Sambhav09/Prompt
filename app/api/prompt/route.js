cf5d5acb628ec6dad0295798575090f71fce61c5
Sambhav jain <jsambhav2004@gmail.com>
2025-03-30 10:32:18 +0530

    new change

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -2,22 +2,22 @@ import { NextResponse } from "next/server";
 import Prompt from "@models/Prompt";
 import { connectToDB } from "@utils/database";
 
-export async function GET(req)  {
+export async function GET(req) {
     const url = new URL(req.nextUrl);
     const tag = url.searchParams.get("tag");
-    
+
     try {
         await connectToDB();
         let filter = {};
-        if(tag) {
-            filter = { tag: { $regex: tag, $options: "i" } };  
+        if (tag) {
+            filter = { tag: { $regex: tag, $options: "i" } };
         }
 
 
-        const prompts = await Prompt.find(filter).populate("userId", "username email image").lean();
+        const prompts = await Prompt.find(filter).populate("userId", "username email image").sort({ createdAt: -1 }).lean();
 
         return NextResponse.json(prompts, { status: 200 });
-    } catch(error) {
+    } catch (error) {
         console.log("Error fetching prompts:", error);
         return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
     }
--- models/Prompt.js
+++ models/Prompt.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const PromptSchema = new mongoose.Schema({
     userId: {
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: "User", 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
         required: true,
     },
     prompt: {
@@ -14,7 +14,7 @@ const PromptSchema = new mongoose.Schema({
         type: String,
         required: true,
     }
-});
+}, { timestamps: true }); // 
 
 const Prompt = mongoose.models.Prompt || mongoose.model("Prompt", PromptSchema);
 
